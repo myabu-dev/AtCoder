be422cc4458daf89fd8c8557e5a44da607e113de
yuu <dptypo@gmail.com>
2019-04-17 21:55:06 +0900

    ABC 121 A

---
+++ ABC/121/A.cpp
@@ -0,0 +1,29 @@
+//
+// Created by yuu on 2019-04-17.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <utility>
+#include <math.h>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+int main(void){
+    int H,W;
+    int h,w;
+
+    cin>>H>>W;
+    cin>>h>>w;
+
+    cout<<(H-h)*(W-w)<<endl;
+    
+
+    return 0;
+}
