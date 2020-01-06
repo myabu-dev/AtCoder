12699d552cea0b1578c285cf6af470e9cbf8d9eb
yuu <dptypo@gmail.com>
2020-01-06 15:16:17 +0900

    ABC098A

--- ABC/098/A.cpp
+++ ABC/098/A.cpp
@@ -2,3 +2,33 @@
 // Created by Myabu on 2020/01/06.
 //
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <utility>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <numeric>
+
+using namespace std;
+
+
+int main(void){
+    int A, B;
+
+    cin>>A>>B;
+
+    int ans;
+    ans = max(A+B, A-B);
+    ans = max (ans, A*B);
+
+    cout<<ans<<endl;
+
+
+    return 0;
+}
+
