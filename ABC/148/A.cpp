d46a2506c603dd5fb3a32b38f9f6c706f87b7b6a
yuu <dptypo@gmail.com>
2020-01-08 23:53:18 +0900

    ABC148 A

---
+++ ABC/148/A.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Myabu on 2020/01/08.
+//
+
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    int A, B;
+    cin>>A>>B;
+    if(A+B==4){
+        cout<<2<<endl;
+    }else if(A+B==3){
+        cout<<3<<endl;
+    }else{
+        cout<<1<<endl;
+    }
+
+    return 0;
+}
