712b875f17d6379c917199210cfe2d3c0b5692c1
yuu <dptypo@gmail.com>
2020-01-10 23:06:11 +0900

    ABC149 A

---
+++ ABC/149/A.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Myabu on 2020/01/10.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <utility>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <numeric>
+
+using namespace std;
+
+
+int main(void){
+    string S, T;
+    cin>>S>>T;
+
+    cout<<T<<S<<endl;
+
+
+    return 0;
+}
