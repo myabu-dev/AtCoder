c27364ed64a8dae8830efafa9aa2770a0c472277
yuu <dptypo@gmail.com>
2020-01-14 01:36:28 +0900

    ABC019 A

---
+++ ABC/019/A.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Myabu on 2020/01/14.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <utility>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <numeric>
+
+using namespace std;
+
+
+int main(void){
+    int x[3];
+
+    cin>>x[0]>>x[1]>>x[2];
+
+    sort(x, x+3);
+
+    cout<<x[1]<<endl;
+
+
+
+
+    return 0;
+}
