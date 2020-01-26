4b9d0d2c7084a2333324cc670ff35bde6d3e5f51
yuu <dptypo@gmail.com>
2020-01-26 15:14:31 +0900

    ABC075 AB

--- ABC/075/B.cpp
+++ ABC/075/B.cpp
@@ -22,22 +22,34 @@ int main(void){
     int H,W;
     string S[55];
 
+    cin>>H>>W;
     for (int i = 0; i < H; ++i) {
         cin>>S[i];
     }
 
-    for (int i = 0; i < H; ++i) {
-        for (int k = 0; k < W; ++k) {
-            if(S[i][k] == '.'){
-                int temp = 0;
-                if(i!=0){
-                    if(S[i-1][k] == '#')temp++;
-                    if(
-                    if(S[i-1][k-1] == '#')temp++;
-                }
+    int dx[] = {1,0,-1,0,1,1,-1,-1};
+    int dy[] = {0,1,0,-1,1,-1,-1,1};
 
+
+    for (int y = 0; y < H; ++y) {
+        for (int x = 0; x < W; ++x) {
+            if(S[y][x] == '#')continue;
+            int count = 0;
+            for (int A = 0; A < 8; ++A) {
+                int ax = x+dx[A];
+                int ay = y+dy[A];
+
+                if(ax<0 || ay<0 || ax>W || ay > H){
+                    continue;
+                }
+                if(S[ay][ax] == '#')count++;
             }
+            S[y][x] = (char)count + '0';
         }
+    }
+
+    for (int i = 0; i < H; ++i) {
+        cout<<S[i]<<endl;
     }
 
 
